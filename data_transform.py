123a48099de53e10045fe0c77617b8d11fbc5e89
talhahaziz <talhahazizkhan@gmail.com>
2025-05-23 17:57:18 +0100

    added category and integer function to transform calss and a summary to print a summary of the df including missing values

--- data_transform.py
+++ data_transform.py
@@ -31,7 +31,6 @@ class DataTransform:
             column (str): The name of the column to convert.
             date_format (str, optional): The format of the date in the column. Defaults to None.
         """
-
         try:
             self.df[column] = pd.to_datetime(self.df[column], format=date_format, errors="coerce")
             print(f"column labelled {column} has successfully been converted to datetime.")
@@ -41,7 +40,6 @@ class DataTransform:
     def convert_to_numeric(self, column: str):
         """
         Converts a column to numeric format.
-
         Args:
             column (str): The name of the column to convert.
         """
@@ -57,10 +55,44 @@ class DataTransform:
 
         Args:
             colums(str): the name of the column i want to remove the symbol from
-            symbol(list): list of the symbols i want to remove
+            symbol(str): the symbol i want to remove
         """
         try:
             self.df[column] = self.df[column].astype(str).str.replace(symbol, '', regex=False) #incase the column type is numeric (float/int) the astype will convert it into a string
             print(f"successfully removed '{symbol}' from the {column} column")
         except Exception as e:
             print(f"Error removing the '{symbol}' from the {column} column: {e}")
+
+    def to_category(self, *columns):
+        """
+        converts specified columns to a category 
+        """
+
+        for column in columns:
+            try:
+                self.df[columns] = self.df[column].astype("category")
+            except Exception as e:
+                print(f"failed to specify categorise '{column}': {e}")
+            
+
+    def to_Int(self, column):
+        """
+        Converts specified columns to integer format, coercing errors to NaN.
+
+        Args:
+            column: Column names to be converted to integer.
+        """
+        try:
+            self.df[column] = pd.to_numeric(self.df[column], errors="coerce").astype("Int64")
+        except Exception as e:
+            print(f"error converting {column} to integer : {e}")
+
+    def summary(self) -> None:
+        """
+        Prints a summary of the DataFrame including data types and missing values.
+        """
+        print("DataFrame Summary:")
+        print(self.df.info())
+        print("\nMissing Values:")
+        print(self.df.isnull().sum())
+
